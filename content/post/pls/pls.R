2d5768018c786c5d9944a2972dd214cbf04c692b
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2022-06-22 18:01:03 +0200

    content: start post on PLS

---
+++ content/post/pls/pls.R
@@ -0,0 +1,108 @@
+# Project:   blogdown
+# Objective: Partial Least Square ideas
+# Author:    Edoardo Costantini
+# Created:   2022-06-13
+# Modified:  2022-06-13
+
+# Load packages ----------------------------------------------------------------
+
+# install pls package (if not already installed)
+install.packages("pls")
+
+# load pls package
+library(pls)
+
+# Fit some linear model with PLS through package
+
+# Preapre data -----------------------------------------------------------------
+
+# 1. Select vairables for model
+dt <- cbind(yarn[[2]], yarn[[1]])
+
+# 2. Scale
+# Means
+round(colMeans(dt), 3)
+
+# 3. Objects needed
+p <- ncol(dt)-1
+n <- nrow(dt)
+
+# Variance
+round(apply(dt, 2, var), 3)
+
+# center and scale to variance = 1 
+dt_sd <- cbind(yarn[[2]], scale(yarn[[1]]))
+
+# Checks
+round(colMeans(dt_sd), 3)
+round(apply(dt_sd, 2, var), 3)
+
+# Estimation -------------------------------------------------------------------
+
+Xmj <- dt_sd[, -1]
+y <- dt_sd[, 1]
+M <- 10 # number of "components"
+y_hat <- cbind(mean(dt_sd[, 1]), matrix(rep(NA, n * (M-1)), nrow = n))
+
+z <- matrix(NA, nrow = n, ncol = M+1)
+theta_hat <- rep(NA, M)
+
+orthogonalize <- function(vec1, vec2){
+    v <- vec1
+    u <- vec2
+
+    newv <- v - drop(t(u) %*% v / (t(u) %*% u)) * u
+
+    return(newv)
+}
+
+for(m in 2:M){
+    # 2a
+    IP <- matrix(NA, nrow = n, ncol = p)
+    for(j in 1:p){
+        IP[, j] <- t(Xmj[, j]) %*% y %*% Xmj[, j]
+    }
+    z[, m] <- rowSums(IP)
+
+    # 2b
+    theta_hat[m] <- drop(z[, m] %*% y / z[, m] %*% z[, m])
+
+    # 2c
+    y_hat[, m] <- y_hat[, m-1] + theta_hat[m] * z[, m]
+
+    # 2d orthogonalize all columns
+    for(j in 1:p){
+        Xmj[, j] <- orthogonalize(Xmj[, j], z[, m])
+    }
+}
+
+# fit PCR model
+model <- plsr(
+    dt[, 1] ~ dt[, -1],
+    ncomp = M,
+    scale = FALSE, 
+    center = FALSE,
+    method = "oscorespls",
+    validation = "none"
+)
+
+round(as.data.frame(fitted(model)), 3)
+
+round(y_hat, 3)
+
+### MIFA
+install.packages("mifa")
+library(mifa)
+mifa::mifa
+
+# Types of DVs -----------------------------------------------------------------
+
+
+
+data(oliveoil)
+sens.pcr <- pcr(sensory ~ chemical, ncomp = 4, scale = TRUE, data = oliveoil)
+sens.pls <- plsr(sensory ~ chemical, ncomp = 4, scale = TRUE, data = oliveoil)
+
+oliveoil$sensory
+
+cppls.fito
